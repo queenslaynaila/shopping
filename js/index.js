1de60d5b266a5ce140e593e4871cc7743b462c45
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-24 15:09:38 +0300

    add functionality to cart

--- js/index.js
+++ js/index.js
@@ -1,35 +1,42 @@
 const itemList = document.querySelector("#item-list");
-
-store.forEach((item, index) => {
+store.forEach((item) => {
   const imageContainer = document.createElement("div");
   imageContainer.setAttribute("class", "col");
+  itemList.appendChild(imageContainer);
 
   const img = document.createElement("img");
-  img.setAttribute("src", `${item.image}`);
+  img.setAttribute("src", item.image);
   imageContainer.appendChild(img);
 
-  const h5 = document.createElement("h3");
+  const h5 = document.createElement("h5");
   h5.textContent = item.item;
+  imageContainer.appendChild(h5);
 
   const p = document.createElement("p");
   p.textContent = item.description;
-
-  const qty = document.createElement("input");
-  qty.setAttribute("value", "");
-  qty.setAttribute("id", index);
-  qty.setAttribute("placeholder", "QTY");
-  qty.setAttribute("type", "number");
-
-  const addToCart = document.createElement("button");
-  addToCart.setAttribute("class", "btn btn-warning btn-sm m-4");
-    addToCart.textContent = "Add to Cart";
-    
-  
-
-  imageContainer.appendChild(h5);
   imageContainer.appendChild(p);
-  imageContainer.appendChild(qty);
-  imageContainer.appendChild(addToCart);
-  itemList.appendChild(imageContainer);
+
+  const inputField = document.createElement("input");
+  inputField.setAttribute("value", " ");
+  inputField.setAttribute("id", "index");
+  inputField.setAttribute("placeholder", "QTY");
+  inputField.setAttribute("type", "number");
+  imageContainer.appendChild(inputField);
+
+  const addToCartbutton = document.createElement("button");
+  addToCartbutton.setAttribute("class", "btn btn-warning btn-sm m-4");
+  addToCartbutton.textContent = "Add to Cart";
+  imageContainer.appendChild(addToCartbutton);
+
+  addToCartbutton.addEventListener("click", () => {
+    summaryParagraph = document.createElement("p");
+    div.appendChild(summaryParagraph);
+    summaryParagraph.textContent = `${inputField.value} ${item.item} bought`;
+  });
 });
 
+const div = document.createElement("div");
+document.body.appendChild(div);
+const h4 = document.createElement("h4");
+h4.textContent = "CART SUMMARY";
+div.appendChild(h4);

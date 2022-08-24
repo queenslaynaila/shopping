e6ba76c85d18753b9343ca10f494da6363800841
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-24 13:39:34 +0300

    code

---
+++ js/data.js
@@ -0,0 +1,21 @@
+const store = [
+  {
+    item: "Shoes",
+    description: "black shoes size 32 color brown, pure leather",
+    price: 3000,
+    image: "https://via.placeholder.com/200",
+  },
+  {
+    item: "Trousers",
+    description: "Jeans size 32 colour navy blue",
+    price: 1500,
+    image: "https://via.placeholder.com/200",
+  },
+  {
+    item: "T-Shirt",
+    description: "Polo T-shirt size XL color black",
+    price: 1000,
+    image: "https://via.placeholder.com/200",
+    },
+  
+];
---
+++ js/index.js
@@ -0,0 +1,38 @@
+const itemList = document.querySelector("#item-list");
+
+store.forEach((item, index) => {
+  const imageContainer = document.createElement("div");
+  imageContainer.setAttribute("class", "col");
+
+  const img = document.createElement("img");
+  img.setAttribute("src", `${item.image}`);
+  imageContainer.appendChild(img);
+
+  const h5 = document.createElement("h3");
+  h5.textContent = item.item;
+
+  const p = document.createElement("p");
+  p.textContent = item.description;
+
+  const qty = document.createElement("input");
+  qty.setAttribute("value", "");
+  qty.setAttribute("id", index);
+  qty.setAttribute("placeholder", "QTY");
+  qty.setAttribute("type", "number");
+
+  const addToCart = document.createElement("button");
+  addToCart.setAttribute("class", "btn btn-warning btn-sm m-4");
+    addToCart.textContent = "Add to Cart";
+    
+    addToCart.addEventListener("click",clickEvent)
+
+  imageContainer.appendChild(h5);
+  imageContainer.appendChild(p);
+  imageContainer.appendChild(qty);
+  imageContainer.appendChild(addToCart);
+  itemList.appendChild(imageContainer);
+});
+
+function clickEvent(e) {
+    document.createElement
+}
